ac47675b2a5b6c96324a4cfc83631a25491a25e8
Olaf Wisselink <olafwisselink@live.nl>
2018-05-02 20:41:20 +0200

    add asound configs

--- play_tussenwater.sh
+++ play_tussenwater.sh
@@ -1,13 +1,8 @@
 #! /bin/bash
 
-echo "Starting in 10 seconds"
-sleep 10
-echo "Starting now"
-
 VIDEOFILE=/home/pi/tussenwater/tw_test.mp4
 
 amixer -c 1
-amixer sset 'Headphone' 50
+#amixer sset 'Headphone' 50
 
-#omxplayer -k
-omxplayer -o alsa -I --loop --layout 2.0 ${VIDEOFILE}
+omxplayer -o alsa --no-osd --loop --layout 2.0 ${VIDEOFILE}
